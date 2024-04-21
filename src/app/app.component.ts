963a8100beb606ba00a5ab84f3d17bc756a502ad
Sujentamang <heysujen@gmail.com>
2024-04-21 22:18:29 +0545

    Debug appCompnent Route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { HomepageComponent } from './pages/homepage/homepage.component';
 import { NavComponent } from './Components/nav/nav.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { BrowserModule } from '@angular/platform-browser';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    Ng2SearchPipeModule,
-    Component,
-    HomepageComponent,
-    NavComponent,
-    RouterModule,
-    RouterOutlet,
-    CommonModule,
-  ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, RouterModule],
+  templateUrl: './app.component.html',
+  styles: '',
 })
 export class AppComponent {}
