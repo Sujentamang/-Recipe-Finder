6ad20e63a2cfdae37a38569d9e4e19b23f87d7c5
Sujentamang <heysujen@gmail.com>
2024-04-21 08:30:44 +0545

    Updated Ui

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -18,12 +18,14 @@ export class ProfileComponent implements OnInit {
       this.recipeList = JSON.parse(localData);
     }
   }
+  isVisual = false;
   showDialog() {
     const dialog = document.getElementById('dialog');
     dialog?.classList.remove('hidden');
     dialog?.classList.add('flex');
   }
   hideDialog() {
+    this.isVisual = true;
     const dialog = document.getElementById('dialog');
     dialog?.classList.add('hidden');
     dialog?.classList.remove('flex');
@@ -41,13 +43,43 @@ export class ProfileComponent implements OnInit {
       localStorage.setItem('RecipeFinder', JSON.stringify(newArr));
     }
   }
+
   onEdit(item: RecipesDetails) {
     this.recipeObj = item;
     this.showDialog();
   }
-  onDelete(item: RecipesDetails) {}
+  update() {
+    const currentRecord = this.recipeList.find(
+      (m) => m.id === this.recipeObj.id
+    );
+    if (currentRecord != undefined) {
+      currentRecord.title = this.recipeObj.title;
+      currentRecord.types = this.recipeObj.types;
+      currentRecord.ingredients = this.recipeObj.ingredients;
+      currentRecord.instructions = this.recipeObj.instructions;
+    }
+    localStorage.setItem('RecipeFinder', JSON.stringify(this.recipeObj));
+  }
+  // onDelete(id: number) {
+  //   const isDelete = confirm('Are you sure want to delete');
+  //   if (isDelete) {
+  //     const currentRecord = this.recipeList.findIndex((e) => e.id === id);
+  //     if (currentRecord !== -1) {
+  //       this.recipeList.shift();
+  //       localStorage.setItem('RecipeFinder', JSON.stringify(this.recipeObj));
+  //     } else {
+  //       console.error('Item to delete not found in recipeList');
+  //     }
+  //   }
+  //     // for (let i = 0; i < this.recipeList.length; i++) {
+  //     //   if (this.recipeList[i].id == id) {
+  //     //     this.recipeList.splice(i, 1);
+  //     //   }
+  //     // }
+  //     localStorage.setItem('RecipeFinder', JSON.stringify(this.recipeObj));
+  //   }
+  // }
 }
-
 export class RecipesDetails {
   id: number;
   title: string;
