bb816eee5a441e35702ddd828e8c806f73cbd7a3
Sujentamang <heysujen@gmail.com>
2024-04-21 08:31:34 +0545

    Change default route

--- src/app/Components/nav/nav.component.ts
+++ src/app/Components/nav/nav.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { AddRecipesComponent } from './Components/add-recipes/add-recipes.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomepageComponent,
   },
   {
